88b1dff59112b1e170d876fcd65ed7e5fded0a2a
greezlu <greezlu@gmail.com>
2020-12-29 20:52:32 +0200

    Lesson 29.12

---
+++ oct_291220/js/main.js
@@ -0,0 +1,8 @@
+// var message = "привет";
+// alert (message);
+
+var answer,
+    question = "Сколько вам лет?";
+
+answer = prompt(question, "Ублюдок, мать твою. А ну, иди сюда!");
+alert(answer);
