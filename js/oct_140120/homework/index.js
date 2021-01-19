ab9ccdeccbf164a4b493f08cb896692813a5aa19
greezlu <greezlu@gmail.com>
2021-01-19 18:07:58 +0200

    Homework 14.01 Fix

--- js/oct_140120/homework/index.js
+++ js/oct_140120/homework/index.js
@@ -1,7 +1,7 @@
 var num = +prompt("Enter your number");
 var checkPass;
 
-checkPass = (num >= 1000 && num <= 2000 && num%2==0 && num%10!=0)
-            || (num%100==0 && num%300!=0);
+checkPass = (num >= 1000 && num <= 2000) && ( (num%2==0 && num%10!=0)
+            || (num%100==0 && num%300!=0) );
 
 alert("Check pass: " + checkPass);
