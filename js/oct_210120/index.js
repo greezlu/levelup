7e985b784269fb66305965501a67f5ccdc784101
greezlu <greezlu@gmail.com>
2021-01-21 21:01:58 +0200

    Lesson 21.01

---
+++ js/oct_210120/index.js
@@ -0,0 +1,89 @@
+/*
+
+Запросить порядковый номер пальца руки
+Проверить введённый номер на допустимость
+Если номер - допустимый: вывести название пальца
+Нумерация - с большого пальца
+
+ */
+
+/*
+
+let fingerNumber = +prompt("Введите номер пальца");
+let fingerName = "Неизвестно какой";
+
+if (fingerNumber == 1) {
+    fingerName = "Большой";
+} else if (fingerNumber == 2) {
+    fingerName = "Указательный";
+} else if (fingerNumber == 3) {
+    fingerName = "Средний";
+} else if (fingerNumber == 4) {
+    fingerName = "Безымянный";
+} else if (fingerNumber == 5) {
+    fingerName = "Мизинец";
+}
+
+alert("Ваш палец: " + fingerName);
+
+*/
+
+/*
+
+let a = 9;
+
+switch (a) {
+    case (1): {
+        alert(1);
+    } case 2: {
+        alert(2);
+    } case (3): {
+        alert(3);
+    } case (4): {
+        alert(4);
+        // break;
+    } case (5):
+        alert(5);
+    default: {
+        alert("default");
+    }
+}
+
+ */
+
+/*
+
+for (let i=1; i<=10; i++) {
+    if (i%2==0) {alert(i)}
+};
+
+*/
+
+
+/*
+let num = 0;
+do {
+    num = prompt("Less than 100: " + num);
+} while (num<100);
+*/
+
+let num = +prompt("Enter number", 10);
+
+for (let i=1; i<num; i++) {
+
+    for (let a=1;a<i;a++) {
+
+    };
+
+};
+
+
+/*
+
+Запросить
+Введите номер команды
+Проверить "на допустимость" (пиздец, не знаю что это блять такое)
+Если проверка пройдена, запросить числа, выполнить действия, вывести результат
+
+ */
+
