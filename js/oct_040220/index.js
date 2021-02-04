4f217073171bf220d7ba7a4b7ed40eb6d98fae67
greezlu <greezlu@gmail.com>
2021-02-04 20:59:02 +0200

    Lesson 04.02

---
+++ js/oct_040220/index.js
@@ -0,0 +1,47 @@
+/*
+function power (num, degree) {
+    let res = 1;
+    for (let i=1; i <= degree; i++) {
+        res *= num;
+    }
+    return res;
+}
+*/
+
+/*
+function random (min, max) {
+    let randomNum = Math.random() * (max-min) + min;
+    return Math.trunc(randomNum);
+}
+
+alert(random(+prompt("",6),+prompt("",10)));
+*/
+
+/*
+Функция принимает длинну и ширину комнаты
+Вычисляет и возвращает площадь комнаты
+*/
+
+/*
+function square (width, length) {
+    return width * length;
+}
+
+alert(square(6,5));
+*/
+
+/*
+Функция получает рост (см) и вес (кг)
+Вычесляет индекс массы тела человека
+160cm
+60kg
+*/
+
+/*
+function massIndex (hight, weidth) {
+    hight /= 100;
+    return weidth / hight**2;
+}
+
+alert(massIndex(160,60));
+*/
