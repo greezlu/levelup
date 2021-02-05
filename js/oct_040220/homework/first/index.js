790a5c3d0cc147561dccbc38a4bb15cc8fa2a18a
greezlu <greezlu@gmail.com>
2021-02-05 21:25:06 +0200

    Homework 04.02

---
+++ js/oct_040220/homework/first/index.js
@@ -0,0 +1,28 @@
+function getBMI (hight, weidth) {
+    hight /= 100;
+    return weidth / hight**2;
+}
+
+function getAdvise (bmi) {
+
+  if(bmi<15){
+    return "Underweight";
+  } else if (bmi<25) {
+    return "Normal";
+  } else if (bmi>=25) {
+    return "Overweight";
+  }
+
+}
+
+function getParams () {
+  let hight = +prompt("Please enter your height");
+  let weidth = +prompt("Please enter your weidth");
+
+  let bmi = getBMI (hight, weidth);
+  let advice = getAdvise (bmi);
+
+  alert("Your weight is: "+advice);
+}
+
+getParams ();
---
+++ js/oct_040220/homework/second/index.js
@@ -0,0 +1,31 @@
+function isPrime (num) {
+
+  let isNumPrime = true;
+
+  for (let i=2;i<num;i++) {
+    if (num%i === 0) {
+      isNumPrime = false;
+      break;
+    }
+  }
+
+  return isNumPrime;
+
+}
+
+function getPrimesFromRange () {
+
+  let from = +prompt("Please enter first number");
+  let to = +prompt("Please enter last number");
+
+  let primeResult = "";
+
+  for (from;from<=to;from++) {
+    if (isPrime (from)) primeResult += from + " ";
+  }
+
+  alert(primeResult);
+
+}
+
+getPrimesFromRange ();
