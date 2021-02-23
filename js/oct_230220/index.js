bbaf873853d15876b9f083663c97dd0530869224
greezlu <greezlu@gmail.com>
2021-02-24 00:01:19 +0200

    Homework unfinished 23.02

--- js/oct_230220/index.js
+++ js/oct_230220/index.js
@@ -59,12 +59,25 @@ alert(getMealCost());
 сортировка массива по возрастанию
 */
 
-console.log(bubbleSort([4,1,6,73,1,7,10,100,3]));
+let newArr = Array();
 
+for (let i=0;i<10000;i++) {
+  newArr.push(Math.random()*100);
+}
+
+// console.log(newArr);
+
+let date = Date.now();
+// console.log(bubbleSort([4,1,6,73,1,7,10,100,3]));
+// console.log( bubbleSort(newArr) );
+bubbleSort(newArr);
+console.log(Date.now() - date);
+
+/*
 function bubbleSort (arr) {
     for (let i=0;i<arr.length;i++) {
         let a = null;
-        for (let k=i;k<arr.length;k++) {
+        for (let k=i;k<arr.length-i;k++) {
             if (arr[k]>arr[k+1]) {
                 a = arr[k];
                 arr[k] = arr[k+1];
@@ -82,4 +95,87 @@ function bubbleSort (arr) {
     }
     return arr;
 }
+*/
+
+
+function bubbleSort (arr) {
+    for (let i=0;i<arr.length;i++) {
+      for (let k=i+1;k<arr.length-i;k++) {
+            if (arr[i]>arr[k]) {
+                let a = arr[i];
+                arr[i] = arr[k];
+                arr[k] = a;
+            }
+        }
+    }
+    return arr;
+}
+
+
+/*
+function bubbleSort (arr) {
+  let reverce = false;
+  for (let i=0;i<arr.length;i++) {
+    if (!reverce){
+      for (let k=i+1;k<arr.length;k++) {
+        if (arr[i]>arr[k]) {
+          let a = arr[i];
+          arr[i] = arr[k];
+          arr[k] = a;
+        }
+      }
+      reverce = true;
+    } else {
+      for (let k=arr.length;k>i;k--) {
+        if (arr[i]>arr[k]) {
+          let a = arr[i];
+          arr[i] = arr[k];
+          arr[k] = a;
+        }
+      }
+      reverce = false;
+    }
+
+    }
+    return arr;
+}
+*/
+
+/*
+function bubbleSort (arr) {
+    for (let i=0;i<arr.length;i++) {
+        let a = null;
+        for (let k=0;k<arr.length-i;k++) {
+            if (arr[k]>arr[k+1]) {
+                a = arr[k];
+                arr[k] = arr[k+1];
+                arr[k+1] = a;
+            }
+        }
+        if (a) arr = bubbleSort(arr);
+    }
+    return arr;
+}
+*/
+
+/*
+function bubbleSort (arr) {
+
+  let a;
+
+  do {
+    a = null;
+    for (let i=0;i<arr.length-i;i++) {
+      if (arr[i]>arr[i+1]) {
+          a = arr[i];
+          arr[i] = arr[i+1];
+          arr[i+1] = a;
+        }
+    }
+  } while (a);
+
+  return arr;
+}
+
+*/
 
