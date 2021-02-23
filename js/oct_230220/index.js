c34e1cdfd176f23d14235a89075810ea3e939f8e
greezlu <greezlu@gmail.com>
2021-02-23 20:58:18 +0200

    Lesson 23.02

---
+++ js/oct_230220/index.js
@@ -0,0 +1,85 @@
+/*
+Обьявить массив numbers
+5 произвольных значений
+Вывести alert
+*/
+
+/*
+let numbers = [3,4,5,6,7];
+
+function getArraySum (arr) {
+    let message = arr[0];
+    let sum = arr[0];
+    for (let i=1; i<arr.length;i++) {
+        message += " + "+arr[i];
+        sum += arr[i];
+    }
+    return message + " = " + sum/arr.length;
+}
+
+alert(getArraySum(numbers));
+*/
+
+/*--------------------------------------*/
+
+/*
+Запросить 5 значений
+Введённые значения сохранять в массиве
+Вывести:
+все значения из массива через заяпятую
+а также их ср.знач.
+*/
+
+/*
+function getMealCost (days = 5) {
+
+    let arr = [];
+
+    for (let i=0;i<days;i++) {
+        arr[i] = +prompt("Please enter your number");
+    }
+
+    let message = arr[0];
+    let sum = arr[0];
+
+    for (let i=1; i<arr.length;i++){
+        message += ", "+arr[i];
+        sum += arr[i];
+    }
+
+    return message + "\n" + "average: " + sum/days;
+
+}
+
+alert(getMealCost());
+*/
+
+/*
+Создать массив на 10 значений
+сортировка массива по возрастанию
+*/
+
+console.log(bubbleSort([4,1,6,73,1,7,10,100,3]));
+
+function bubbleSort (arr) {
+    for (let i=0;i<arr.length;i++) {
+        let a = null;
+        for (let k=i;k<arr.length;k++) {
+            if (arr[k]>arr[k+1]) {
+                a = arr[k];
+                arr[k] = arr[k+1];
+                arr[k+1] = a;
+            }
+        }
+        for (let j=arr.length-i;j>=i;j--) {
+            if (arr[j]<arr[j-1]) {
+                a = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = a;
+            }
+        }
+        if (a === null) break;
+    }
+    return arr;
+}
+
