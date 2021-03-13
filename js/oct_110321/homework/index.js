f1360dcc4901de38ee6969dd7b91c5ca3de337e6
greezlu <greezlu@gmail.com>
2021-03-13 23:06:17 +0200

    Homework 11.03

--- js/oct_110321/homework/index.js
+++ js/oct_110321/homework/index.js
@@ -1,8 +1,52 @@
-let start = +prompt("Please enter first number");
-let end = +prompt("Please enter second number");
+/*
+Запросить у пользователя начало и окончание промежутка
+целых чисел (первое меньше второго).
+Сгенерировать таблицу Пифагора в данном промежутке.
+*/
 
-let pyth = tablePythagoras(start,end);
-document.body.append(pyth);
+let data = getInputData();
+let pyth;
+
+if (typeof data === "string") {
+  alert(data);
+} else if (typeof data === "object") {
+  let pyth = tablePythagoras(data.start, data.end);
+  document.body.append(pyth);
+}
+
+
+function getInputData () {
+
+  let start = prompt("Please enter first number");
+
+  if (start === null) {
+    return "Operation canceled";
+  } else if (!isFinite(start)) {
+    return "Incorrect number";
+  } else {
+    start = Number(start);
+  }
+
+  let end = prompt("Please enter second number");
+
+  if (end === null) {
+    return "Operation canceled";
+  } else if (!isFinite(end)) {
+    return "Incorrect number";
+  } else {
+    end = Number(end);
+  }
+
+  if (end <= start) {
+    return "Second number must be more than first";
+  }
+
+  return {
+    start: start,
+    end: end
+  }
+
+}
 
 function tablePythagoras (start, end) {
 
