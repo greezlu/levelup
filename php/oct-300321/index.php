ef9f8a61b8129f43f8388f90205fca22d23ac3ec
greezlu <greezlu@gmail.com>
2021-03-31 11:45:38 +0300

    Homework 30.03

--- php/oct-300321/index.php
+++ php/oct-300321/index.php
@@ -1,3 +1,49 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>oct-300321</title>
+  <style media="screen">
+    * {
+      margin: 0;
+      padding: 0;
+      border: 0;
+    }
+    table {
+      border-collapse: collapse;
+      margin: 0 auto;
+    }
+    table, tr, td {
+      border: 1px solid black;
+    }
+    td {
+      padding: 5px 8px;
+    }
+  </style>
+</head>
+<body>
+  <table>
+    <tbody>
+      <?php
+      $list = [
+        ['Vasya','Pupko'],
+        ['Kolya','Dudko'],
+        ['Olya','Banko'],
+        ['Stas','Dubko'],
+      ];
 
-echo "hello world";
+      $size = count($list);
+
+      for ($i=0;$i<$size;$i++) {?>
+
+      <tr>
+        <td><?=$i+1?></td>
+        <td><?=$list[$i][0]?></td>
+        <td><?=$list[$i][1]?></td>
+      </tr>
+
+      <?php } ?>
+    </tbody>
+  </table>
+</body>
+</html>
